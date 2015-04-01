9389b984dde20eed6a96785e4b3eb11673fce165
ItzJ3rmu <jere.santanen@gmail.com>
2015-04-01 10:49:05 +0300

    Oikea
    Oikea versio ampparit ohjelmasta

--- ampparit.py
+++ ampparit.py
@@ -10,13 +10,25 @@ def nouda( url , out ):
 	r.encoding = 'UTF-8'
 	soup = BeautifulSoup( r.text )
 
-        aika = datetime.date.today
+	aika = datetime.date.today()
 
-	teksti = soup.find_all( class_='news-item-wrapper' )
+	numero = raw_input("Kuinka monta artikkelia? ")
 
-	for string in teksti[0].stripped_strings:
-	        out.write(repr(string))
+	a = int(numero)
+
+	for x in range(0,a):
+		#teksti = soup.find_all( class_='news-item-wrapper' )
+		linkki = soup.find_all('a', class_='news-item-headline')
+		aika = soup.find_all( class_='news-item-date col')
+
+		for string in linkki[x].stripped_strings:
+	        	out.write(repr(string))
+
+
+		for string in aika[x + 1].stripped_strings:
+			out.write(repr(string))
+
 
 if __name__ == '__main__':
 
-	nouda("http://yle.fi/uutiset/nordea_synkkyys_jatkuu/7663512", file('yle.txt', 'w'))
+	nouda("http://www.ampparit.com/eduskuntavaalit", file('ampparit.txt', 'w'))
